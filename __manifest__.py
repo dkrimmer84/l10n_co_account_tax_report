736e3fb88d3e0b56ebac0cfc39fd024dcd7e15ce
dkrimmer84 <dominic.krimmer@gmail.com>
2019-10-31 13:17:12 +0000

    preparation for 12.0

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# Part of Probuse Consulting Service Pvt Ltd. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'Inherit Account Print Tax Report',
+    'version' : '12.0.0.0.1',
+    'author' : '',
+    'license': '',
+    'category' : 'Accounting & Finance',
+    'summary': 'Inherit - Account Print Tax Report',
+    'description' : """
+
+    """,
+    'website': '',
+    'depends' : ['account', 'account_tax_report'],
+    'data': ['report/reporttax_view.xml',
+    'views/tax_report_view.xml'],
+    'installable': True,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
